d27ba3c26efa438a9d6b1e95acf1252655a7e7a6
DCathal <121510706+DCathal@users.noreply.github.com>
2025-05-27 14:11:03 +0100

    Add files via upload

---
+++ roam-cathal.js
@@ -0,0 +1,22 @@
+(function() {
+  var div = document.createElement('div');
+  div.id = 'roam-lobby';
+  div.style.minWidth = '320px';
+  document.currentScript.parentNode.insertBefore(div, document.currentScript);
+
+  var script = document.createElement('script');
+  script.src = 'https://ro.am/lobbylinks/embed.js';
+  script.onload = function() {
+    Roam.initLobbyEmbed({
+      url: 'https://ro.am/cathal/',
+      parentElement: document.getElementById('roam-lobby'),
+      lobbyConfiguration: 'booking_only',
+      accentColor: '#0059DC',
+      theme: 'dark',
+      onSizeChange: function(w, h) {
+        div.style.height = h + 'px';
+      }
+    });
+  };
+  document.head.appendChild(script);
+})();
