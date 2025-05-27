5614956885ca15c9af5723b62f4e5f3b456d67c6
DCathal <121510706+DCathal@users.noreply.github.com>
2025-05-27 16:24:39 +0100

    Update roam-fcrmedia.js

--- roam-fcrmedia.js
+++ roam-fcrmedia.js
@@ -1,28 +1,26 @@
 (function() {
-  document.addEventListener("DOMContentLoaded", function () {
-    var div = document.createElement('div');
-    div.id = 'roam-lobby';
-    div.style.minWidth = '320px';
-    div.style.margin = '2rem auto';
-    div.style.maxWidth = '800px';
-    document.body.appendChild(div);
+  var div = document.createElement('div');
+  div.id = 'roam-lobby';
+  div.style.minWidth = '320px';
+  div.style.margin = '2rem auto';
+  div.style.maxWidth = '800px';
+  document.currentScript.parentNode.insertBefore(div, document.currentScript);
 
-    var script = document.createElement('script');
-    script.src = 'https://ro.am/lobbylinks/embed.js';
-    script.onload = function() {
-      Roam.initLobbyEmbed({
-        url: 'https://ro.am/fcrmedia/',
-        parentElement: document.getElementById('roam-lobby'),
-        lobbyConfiguration: 'full',
-        accentColor: '#0059DC',
-        theme: 'dark',
-        onSizeChange: function(w, h) {
-          div.style.height = h + 'px';
-        }
-      });
-    };
-    document.head.appendChild(script);
-  });
+  var script = document.createElement('script');
+  script.src = 'https://ro.am/lobbylinks/embed.js';
+  script.onload = function() {
+    Roam.initLobbyEmbed({
+      url: 'https://ro.am/fcrmedia/',
+      parentElement: document.getElementById('roam-lobby'),
+      lobbyConfiguration: 'full',
+      accentColor: '#0059DC',
+      theme: 'dark',
+      onSizeChange: function(w, h) {
+        div.style.height = h + 'px';
+      }
+    });
+  };
+  document.head.appendChild(script);
 })();
 
 
