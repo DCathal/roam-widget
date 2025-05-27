9f731bcea7019ead0f9e7d18508ed7acf8fceb59
DCathal <121510706+DCathal@users.noreply.github.com>
2025-05-27 15:20:34 +0100

    new lobby

--- roam-cathal.js
+++ roam-cathal.js
@@ -1,22 +1,26 @@
-(function() {
-  var div = document.createElement('div');
-  div.id = 'roam-lobby';
-  div.style.minWidth = '320px';
-  document.currentScript.parentNode.insertBefore(div, document.currentScript);
-
-  var script = document.createElement('script');
-  script.src = 'https://ro.am/lobbylinks/embed.js';
-  script.onload = function() {
-    Roam.initLobbyEmbed({
-      url: 'https://ro.am/fcrmedia/',
-      parentElement: document.getElementById('roam-lobby'),
-      lobbyConfiguration: 'booking_only',
-      accentColor: '#0059DC',
-      theme: 'dark',
-      onSizeChange: function(w, h) {
-        div.style.height = h + 'px';
-      }
-    });
-  };
-  document.head.appendChild(script);
-})();
+<!-- Roam inline widget begin -->
+<div id="roam-lobby" style="min-width: 320px;"></div>
+<script type="text/javascript" src="https://ro.am/lobbylinks/embed.js"></script>
+<script>
+  const parentElement = document.getElementById("roam-lobby");
+  Roam.initLobbyEmbed({
+    url: "https://ro.am/fcrmedia/",
+    parentElement,
+    accentColor: "#0059DC",
+    theme: "dark",
+    lobbyConfiguration: "default",
+    prefill: {
+      name: "Cathal Dempsey",
+      email: "cathal.dempsey@fcrmedia.ie",
+      note: "Howdy!"
+    },
+    onDateTimeSelected: (lobby, payload) => console.debug("onDateTimeSelected", lobby, payload),
+    onProfileFormComplete: (lobby, payload) =>
+      console.debug("onProfileFormComplete", lobby, payload),
+    onEventScheduled: (lobby, payload) => console.debug("onEventScheduled", lobby, payload),
+    onSizeChange: (width, height) => {
+      parentElement.style.height = `${height}px`;
+    },
+  });
+</script>
+<!-- Roam inline widget end -->
