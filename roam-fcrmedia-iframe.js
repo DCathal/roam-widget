81f210604aada89f772f39fe7c5c30fef7387015
DCathal <121510706+DCathal@users.noreply.github.com>
2025-05-27 16:00:22 +0100

    Update roam-fcrmedia-iframe.js

--- roam-fcrmedia-iframe.js
+++ roam-fcrmedia-iframe.js
@@ -1,8 +1,8 @@
 (function() {
   var iframe = document.createElement('iframe');
   iframe.src = 'https://ro.am/fcrmedia/';
-  iframe.style.width = '100%';
-  iframe.style.height = '800px';
+  iframe.style.width = '120%';
+  iframe.style.height = '1200px';
   iframe.style.border = '0';
   iframe.allow = 'camera; microphone; autoplay';
   iframe.setAttribute('title', 'Live Chat with FCR Media');
