7aa97eb3080a9b6a0f527f640baff886079f97ac
DCathal <121510706+DCathal@users.noreply.github.com>
2025-05-27 15:51:26 +0100

    Add files via upload

---
+++ roma-fcrmedia-iframe.js
@@ -0,0 +1,17 @@
+(function() {
+  var iframe = document.createElement('iframe');
+  iframe.src = 'https://ro.am/fcrmedia/';
+  iframe.style.width = '100%';
+  iframe.style.height = '600px';
+  iframe.style.border = '0';
+  iframe.allow = 'camera; microphone; autoplay';
+  iframe.setAttribute('title', 'Live Chat with FCR Media');
+
+  // Optional: Add a container div
+  var container = document.createElement('div');
+  container.style.maxWidth = '500px';
+  container.style.margin = '0 auto';
+  container.appendChild(iframe);
+
+  document.currentScript.parentNode.insertBefore(container, document.currentScript);
+})();
